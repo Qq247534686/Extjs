3a42931798ca0fe8104e048f2a2fe10461352978
LJC <247534686@qq.com>
2017-04-17 20:09:25 +0800

    extjs

---
+++ Extjs4_0/Models/grid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Extjs4_0.Models
+{
+    public class grid
+    {
+        public string common { get; set; }
+        public string light { get; set; }
+        public int price { get; set; }
+        public string availDate { get; set; }
+        public bool Indoor { get; set; }
+    }
+}
---
+++ Extjs4_0/Models/user.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Extjs4_0.Models
+{
+    public class user
+    {
+        public string name { get; set; }
+        public string emali { get; set; }
+    }
+}
